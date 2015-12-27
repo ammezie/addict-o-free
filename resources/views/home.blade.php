de6aacc2ea16c3127cc5cc300198c34b4904623c
mezie <meziemichael@gmail.com>
2015-12-27 15:30:17 +0100

    Setup authentication scaffolding

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,21 +1,17 @@
-@extends('layout')
-@section('title')
-<title>Addict-O-Free</title>
-@stop
-@section('content')
-<main id="cd-main-content">
-  <section id="cd-intro">
-  <div id="button-g">
-    <button type="button" class="button large-button">Login</button>
-    <button type="button" class="button large-button">Register</button>
-  </div>
-    <header class="cd-header">
-      <div id="cd-logo"><a href="#0"><img src="{{ asset('img/cd-logo.svg') }}" alt="Logo"></a></div>
-      <a class="cd-menu-trigger" href="#main-nav">Menu<span></span></a>
-    </header>
-    <div class="cd-blurred-bg"></div>
-  </section> <!-- cd-intro -->
-</main>
+@extends('layouts.app')
 
-<div class="cd-shadow-layer"></div>
-@stop
+@section('content')
+<div class="container spark-screen">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
+
+                <div class="panel-body">
+                    You are logged in!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
