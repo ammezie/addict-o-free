452ec5a69e463fc47b60b179706722bfe8d29e94
Oshosanya Michael <michaeloshosanya@gmail.com>
2015-12-29 20:06:09 +0100

    Added Project Desc

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,7 +15,7 @@
 				<!-- mp-menu -->
 				<nav id="mp-menu" class="mp-menu">
 					<div class="mp-level">
-						<h2 class="icon icon-world">Menu</h2>
+						<h2 class="icon icon-home">Menu</h2>
 						<ul>
 							<li class="icon icon-arrow-left">
 								<a class="icon icon-display" href="#">Devices</a>
@@ -26,6 +26,7 @@
 							</li>
 							<li><a class="icon icon-photo" href="#">Collections</a></li>
 							<li><a class="icon icon-wallet" href="#">Credits</a></li>
+							<li><a class="icon icon-wallet" href="#">Logout</a></li>
 						</ul>
 							
 					</div>
@@ -46,6 +47,11 @@
 
 			</div><!-- /pusher -->
 		</div><!-- /container -->
+
+		<div class="main-content">
+			<h3>We wish to help you with your addiction. Please fill the </h3>
+			<div id="calendar" class="fc-calendar-container"></div>
+		</div>
 <script src="{{ asset('js/classie.js') }}"></script>
 <script src="{{ asset('js/mlpushmenu.js') }}"></script>
 <script>
